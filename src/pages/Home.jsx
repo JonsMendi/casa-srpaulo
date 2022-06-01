1c3a8dede57d1126f6dc4d1d41c9caed6bb3d48b
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-06-01 16:42:09 +0200

    updated title name
    If applied, this commit will have updated the title name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import  { useNavigate } from 'react-router-dom';
 
 function Home () {
+    //this variable contains the navigation from react-router. Anytime that we want to create links independent from the Routes, we use useNavigation()
+    let navigate = useNavigate();
+
+
     return (
         <div>
             <h1>Home</h1>
