9210c9992b9becaff41d6d1c223d8ea6efc7acc5
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-06-07 22:44:10 +0200

    updated Style/ Structure of the web page
    If applied, this commit will have made some updates giving a cleaner styling/structure and adding some placeholders.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,9 +8,26 @@ function About () {
         <Container className="about-container">
            <div className="about-content" id="about">
                 <h1 className="transparent-bg">About</h1>
-                <p className="transparent-bg">What are we?</p>
-                <p className="transparent-bg">What we offer?</p>
-                <p className="transparent-bg">Why us?</p>
+                <div className="explanation-wrapper">
+                   <div className="we">
+                    <p className="transparent-bg">What are we?</p>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                        architecto beatae vitae dicta sunt explicabo.</p> 
+                    </div>
+                    <div className="offer">
+                    <p className="transparent-bg">What we offer?</p>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                        architecto beatae vitae dicta sunt explicabo.</p> 
+                    </div>
+                    <div className="why">
+                    <p className="transparent-bg">Why us?</p> 
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+                        architecto beatae vitae dicta sunt explicabo.</p>   
+                    </div> 
+                </div>
             </div>
         </Container> 
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,20 +8,25 @@ function Home () {
     return (
         <Container className="home-container" id="home">
             <div className="home-content">
-                <h1 className="home-title">Casa do Sr. Paulo</h1>
-                 <div className="bubble"></div>
-                <ButtonContact 
-                    className='btns'
-                    buttonStyle='btn--outline'
-                    buttonSize='btn--large'
-                    >
-                    Reserve</ButtonContact>
-                <ButtonGallery 
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'
-                    >
-                    Watch the House</ButtonGallery>
+                <div className="all-content-wrapper"></div>
+                <div className="content-wrapper">
+                   <h1 className="home-title">Casa do Sr. Paulo</h1>
+                    <div className="button-wrapper">
+                        <ButtonContact 
+                            className='btn'
+                            buttonStyle='btn--outline'
+                            buttonSize='btn--large'
+                        >
+                        Reserve</ButtonContact>
+                        <ButtonGallery 
+                            className='btn'
+                            buttonStyle='btn--primary'
+                            buttonSize='btn--large'
+                        >
+                        Watch the House</ButtonGallery> 
+                    </div> 
+                </div>
+                <div className="bubble"></div>
             </div>
            
             {/* <div className="curve"></div> */}
