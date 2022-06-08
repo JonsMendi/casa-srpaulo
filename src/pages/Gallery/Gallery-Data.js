2a1277c5e82e2858c779a441de302ef7914aca21
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-06-08 15:59:49 +0200

    created a image slider
    If applied, this commit will have created a image slider that displays images from the Gallery-Data.js.
    
    Gallery-Data.js should contain in present and future all the images importations.

---
+++ src/pages/Gallery/Gallery-Data.js
@@ -0,0 +1,18 @@
+export const GalleryData = [ 
+    {
+        id: 1,
+        image: 'https://media.istockphoto.com/photos/billboard-mock-up-in-city-background-beautiful-sunshine-and-bright-picture-id1395728996?b=1&k=20&m=1395728996&s=170667a&w=0&h=ia_dFlGxYd4ggN3xt2gkbdQXK2solX-oF7WYArHpygo='
+    },
+    {
+        id: 2,
+        image: 'https://media.istockphoto.com/photos/empty-lightbox-on-the-street-of-a-city-billboard-placeholder-mockup-picture-id1400166154?b=1&k=20&m=1400166154&s=170667a&w=0&h=ejVOG6pNwQ23Mg0IdqL1jcazz7Zo7UNXQdz3DGuwskg='
+    },
+    {
+        id: 3,
+        image: 'https://media.istockphoto.com/photos/dog-cardboar-neutral-on-member-of-bone-gang-picture-id187895300?b=1&k=20&m=187895300&s=170667a&w=0&h=Cikee4baMVe3JW0VqfAc3o7LLFDcGGx32HvwbkdMVaM='
+    },
+    {
+        id: 4,
+        image: 'https://images.unsplash.com/photo-1596887245124-5150ad2491e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGxhY2Vob2xkZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+    },
+ ]
--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
+import { GalleryData } from '../Gallery/Gallery-Data';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 import './Gallery.css';
 
-function Gallery () {
+function Gallery ({ slides }) {
+    const [current, setCurrent] = useState(0);
+    const slidesLength = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === slidesLength - 1 ? 0 : current + 1);
+    };
+
+    const previousSlide = () => {
+        setCurrent(current === 0 ? slidesLength - 1 : current - 1);
+    };
+
+    if(!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+
     return (
         <Container className="gallery-container">
             <div className='gallery-content' id='gallery'>
                 <h1>Gallery</h1>
-                <p>Photo 1</p>
-                <p>Photo 2</p>
-                <p>Photo 3</p>
+                <div className='gallery-slider'>
+                    <FaArrowAltCircleLeft className='gallery-arrow_left' onClick={previousSlide}/>
+                    <FaArrowAltCircleRight className='gallery-arrow_right' onClick={nextSlide}/>
+                   {GalleryData.map((slide, index) => {
+                       return (
+                           <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                               {index === current && (<img src={slide.image} className='gallery-image' alt='carousel with house views'/>)}
+                           </div>
+                       )
+                   })}
+                </div>
             </div> 
         </Container>
         
